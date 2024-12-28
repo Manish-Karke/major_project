8e561e205c53ae06df8f59eca339bcfcdaf25930
Manish karki <karkimanish188@gmail.com>
2024-12-28 12:25:17 +0545

    fg

--- ielts-essay-app/tailwind.config.js
+++ ielts-essay-app/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  safelist: [
+    "text-2xl",
+    "text-3xl",
+    {
+      pattern: /bg-(red|green|blue)-(100|200|300)/,
+    },
+  ],
+  // ...
+};
